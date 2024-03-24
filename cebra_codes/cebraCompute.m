22329aec734ccca0505ffa4d3dd0b1d4d0cd4fdc
Francesco Donnarumma <francesco.donnarumma@gmail.com>
2024-03-24 03:04:10 +0100

    clean cebra

--- cebra_codes/cebraCompute.m
+++ cebra_codes/cebraCompute.m
@@ -1,11 +1,11 @@
-function [data_trials,out]   = cebraCompute(data_trials,params)
-% function [data_trials,out] = cebraCompute(data_trials,par)
-execinfo    = params.exec;
+function   [data_trials,out] = cebraCompute(data_trials,params)
+% function [data_trials,out] = cebraCompute(data_trials,params)
+execinfo            = params.exec;
 if ~isempty(execinfo); t=tic; fprintf('Function: %s ',mfilename); end
-InField = params.InField;
+InField             = params.InField;
 %% matrici vuote per mettere dati ratto
-rat_n       =[];
-rat_b       =[];
+rat_n               =[];
+rat_b               =[];
 % reconstruct trials
 nTrials     =length(data_trials);
 for iTrial=1:nTrials
@@ -13,23 +13,24 @@ for iTrial=1:nTrials
     rat_b=[rat_b; data_trials(iTrial).labels'];
 end
 
-save('params.mat', 'params');
-save('rat_n.mat', 'rat_n');
-save('rat_b.mat', 'rat_b');
+script_fit          = params.script_fit;
+script_input_dir    = params.script_input_dir;
+script_output_dir   = params.script_output_dir;
 
-script_name     = params.script_name;
-script_out_dir  = params.script_out_dir;
-matlab_out_dir  = params.matlab_out_dir;
+save([script_input_dir 'params.mat'], 'params');
+save([script_input_dir 'rat_n.mat'], 'rat_n');
+save([script_input_dir 'rat_b.mat'], 'rat_b');
+
 %% run fit in python
-command         = sprintf('python "%s" "%s" "%s"', script_name, script_out_dir, matlab_out_dir);
+command             = sprintf('python "%s" "%s" "%s"', script_fit, script_input_dir, script_output_dir);
 fprintf('%s ',command);
-[~, cmdout]     = system(command);
+[~, cmdout]         = system(command);
 fprintf('%s',cmdout);
-out.model_weghts = load(fullfile(matlab_out_dir, 'model_struct.mat'));
+out.model_weghts    = load(fullfile(script_output_dir, 'model_struct.mat'));
 
-cebra_output = load(fullfile(matlab_out_dir, 'cebra_output.mat'));
-out.cebra_output=cebra_output.cebra_output';
-out.fitted_model_p = 'fitted_model.pkl';
+cebra_output        = load(fullfile(script_output_dir, 'cebra_output.mat'));
+out.cebra_output    = cebra_output.cebra_output';
+out.model_file      = [script_output_dir, 'fitted_model.pkl'];
 if ~isempty(execinfo); out.exectime=toc(t); fprintf('| Time Elapsed: %.2f s\n',out.exectime); end
 
 end
